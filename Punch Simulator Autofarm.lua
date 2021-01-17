b721dc244370a6e2126a8b151c03ec2db8e0cb47
qANOXq <77577467+qANOXq@users.noreply.github.com>
2021-01-17 22:29:45 +0100

    Update Punch Simulator Autofarm.lua

--- Punch Simulator Autofarm.lua
+++ Punch Simulator Autofarm.lua
@@ -7,6 +7,7 @@ local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local Frame_2 = Instance.new("Frame")
 local TextButton = Instance.new("TextButton")
+local TextButton_2 = Instance.new("TextButton")
 getgenv().farmer = false;
 
 --Properties:
@@ -15,48 +16,74 @@ ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.185477525, 0, 0.320638835, 0)
-Frame.Size = UDim2.new(0, 352, 0, 19)
+Frame.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Frame.Position = UDim2.new(0.299760193, 0, 0.344070286, 0)
+Frame.Size = UDim2.new(0, 213, 0, 20)
 
 Frame_2.Parent = Frame
 Frame_2.BackgroundColor3 = Color3.fromRGB(59, 59, 59)
 Frame_2.BorderSizePixel = 0
-Frame_2.Position = UDim2.new(0, 0, 1, 0)
-Frame_2.Size = UDim2.new(0, 352, 0, 100)
+Frame_2.Position = UDim2.new(0, 0, 1.00000012, 0)
+Frame_2.Size = UDim2.new(0, 213, 0, 88)
 
 TextButton.Parent = Frame_2
 TextButton.BackgroundColor3 = Color3.fromRGB(127, 127, 127)
-TextButton.Position = UDim2.new(0.0284090918, 0, 0.0900000036, 0)
-TextButton.Size = UDim2.new(0, 331, 0, 75)
+TextButton.Position = UDim2.new(0.0281690136, 0, 0.215909094, 0)
+TextButton.Size = UDim2.new(0, 200, 0, 50)
 TextButton.Font = Enum.Font.Highway
-TextButton.Text = "Start/Stop AutoFarm"
+TextButton.Text = "Start/Stop Autofarm"
 TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.TextSize = 20.000
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
 TextButton.MouseButton1Click:Connect(function()
 	if getgenv().farmer == false then
-	getgenv().farmer = true
+		getgenv().farmer = true
 	else
-	getgenv().farmer = false
+		getgenv().farmer = false
 	end
 end)
 
--- Scripts:
+TextButton_2.Parent = Frame
+TextButton_2.BackgroundColor3 = Color3.fromRGB(127, 127, 127)
+TextButton_2.Position = UDim2.new(0.915492952, 0, -0.0340912342, 0)
+TextButton_2.Size = UDim2.new(0, 18, 0, 18)
+TextButton_2.Font = Enum.Font.Highway
+TextButton_2.Text = "X"
+TextButton_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.TextScaled = true
+TextButton_2.TextSize = 14.000
+TextButton_2.TextWrapped = true
+TextButton_2.MouseButton1Click:Connect(function()
+	ScreenGui:Destroy()
+end)
 
-local function ZRMABIN_fake_script() -- Frame.Dragify 
+
+while wait() do
+	if getgenv().farmer == true then
+		wait(1.3)
+		game:GetService("ReplicatedStorage").Events.GiverPower_Client_Event:FireServer()
+		wait(0.1)
+		game:GetService("ReplicatedStorage").Events.GiverSpeed_Client_Event:FireServer()
+	end    
+end
+
+
+
+local function VBNUYH_fake_script() -- Frame.Dragify 
 	local script = Instance.new('LocalScript', Frame)
 
 	local UIS = game:GetService("UserInputService")
 	function dragify(Frame)
 	    dragToggle = nil
-	    local dragSpeed = 0.50
+	    local dragSpeed = 0
 	    dragInput = nil
 	    dragStart = nil
 	    local dragPos = nil
 	    function updateInput(input)
 	        local Delta = input.Position - dragStart
 	        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
-	        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.30), {Position = Position}):Play()
+	        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
 	    end
 	    Frame.InputBegan:Connect(function(input)
 	        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
@@ -84,15 +111,4 @@ local function ZRMABIN_fake_script() -- Frame.Dragify
 	
 	dragify(script.Parent)
 end
-coroutine.wrap(ZRMABIN_fake_script)()
-
-
-
-while wait() do
-	if getgenv().farmer == true then
-		wait(1.3)
-		game:GetService("ReplicatedStorage").Events.GiverPower_Client_Event:FireServer()
-		wait(0.1)
-		game:GetService("ReplicatedStorage").Events.GiverSpeed_Client_Event:FireServer()
-	end    
-end
+coroutine.wrap(VBNUYH_fake_script)()
