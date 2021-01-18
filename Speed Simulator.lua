70b979cb085d4823a527e991398547123b3618e2
qANOXq <77577467+qANOXq@users.noreply.github.com>
2021-01-18 10:54:24 +0100

    Update Speed Simulator.lua

--- Speed Simulator.lua
+++ Speed Simulator.lua
@@ -1,54 +1,59 @@
+-- Gui to Lua
+-- Version: 3.1
 
+-- Instances:
 
-		local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local Frame_2 = Instance.new("Frame")
+local speedautofarm = Instance.new("ScreenGui")
+local top = Instance.new("Frame")
+local main = Instance.new("Frame")
 local start = Instance.new("TextButton")
 local tp = Instance.new("TextButton")
-local stop = Instance.new("TextButton")
-local TextLabel = Instance.new("TextLabel")
+local x = Instance.new("TextButton")
+getgenv().farmer = false;
 
 --Properties:
 
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+speedautofarm.Name = "speed autofarm"
+speedautofarm.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(27, 27, 27)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.0962904468, 0, 0.126535624, 0)
-Frame.Size = UDim2.new(0, 355, 0, 18)
+top.Name = "top"
+top.Parent = speedautofarm
+top.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
+top.BorderColor3 = Color3.fromRGB(27, 42, 53)
+top.Position = UDim2.new(0.299760193, 0, 0.344070286, 0)
+top.Size = UDim2.new(0, 347, 0, 20)
 
-Frame_2.Parent = Frame
-Frame_2.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
-Frame_2.BorderSizePixel = 0
-Frame_2.Position = UDim2.new(-0.000712728128, 0, 0.979525089, 0)
-Frame_2.Size = UDim2.new(0, 355, 0, 111)
+main.Name = "main"
+main.Parent = top
+main.BackgroundColor3 = Color3.fromRGB(59, 59, 59)
+main.BorderSizePixel = 0
+main.Position = UDim2.new(0, 0, 1, 0)
+main.Size = UDim2.new(0, 348, 0, 88)
 
 start.Name = "start"
-start.Parent = Frame_2
-start.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
-start.BorderSizePixel = 0
-start.Position = UDim2.new(0.0309859179, 0, 0.102702707, 0)
-start.Size = UDim2.new(0, 104, 0, 38)
+start.Parent = main
+start.BackgroundColor3 = Color3.fromRGB(127, 127, 127)
+start.Position = UDim2.new(0.0281690396, 0, 0.215909094, 0)
+start.Size = UDim2.new(0, 154, 0, 50)
 start.Font = Enum.Font.Highway
-start.Text = "Start AutoFarm"
+start.Text = "Start/Stop Autofarm"
 start.TextColor3 = Color3.fromRGB(255, 255, 255)
 start.TextScaled = true
 start.TextSize = 14.000
 start.TextWrapped = true
 start.MouseButton1Click:Connect(function()
-	while wait() do
-		game.Workspace.Events.AddBite1:FireServer("123ase")
-		wait(0.1)
+	if getgenv().farmer == false then
+		getgenv().farmer = true
+	else
+		getgenv().farmer = false
 	end
 end)
 
 tp.Name = "tp"
-tp.Parent = Frame_2
-tp.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
-tp.BorderSizePixel = 0
-tp.Position = UDim2.new(0.681690156, 0, 0.102702707, 0)
-tp.Size = UDim2.new(0, 104, 0, 38)
+tp.Parent = main
+tp.BackgroundColor3 = Color3.fromRGB(127, 127, 127)
+tp.Position = UDim2.new(0.525295496, 0, 0.215909094, 0)
+tp.Size = UDim2.new(0, 154, 0, 50)
 tp.Font = Enum.Font.Highway
 tp.Text = "Sell Teleport"
 tp.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -61,82 +66,72 @@ tp.MouseButton1Click:Connect(function()
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-386.535461, 54.2045021, -1459.29492)
 end)
 
-stop.Name = "stop"
-stop.Parent = Frame_2
-stop.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
-stop.BorderSizePixel = 0
-stop.Position = UDim2.new(0.352112681, 0, 0.102702692, 0)
-stop.Size = UDim2.new(0, 104, 0, 38)
-stop.Font = Enum.Font.Highway
-stop.Text = "Rejoin"
-stop.TextColor3 = Color3.fromRGB(255, 255, 255)
-stop.TextScaled = true
-stop.TextSize = 14.000
-stop.TextWrapped = true
-stop.MouseButton1Click:Connect(function()
-	local ts = game:GetService("TeleportService")
-
-	local p = game:GetService("Players").LocalPlayer
-
-
-
-	ts:Teleport(game.PlaceId, p)
+x.Name = "x"
+x.Parent = top
+x.BackgroundColor3 = Color3.fromRGB(127, 127, 127)
+x.Position = UDim2.new(0.947193265, 0, -0.0340912342, 0)
+x.Size = UDim2.new(0, 18, 0, 18)
+x.Font = Enum.Font.Highway
+x.Text = "X"
+x.TextColor3 = Color3.fromRGB(255, 255, 255)
+x.TextScaled = true
+x.TextSize = 14.000
+x.TextWrapped = true
+x.MouseButton1Click:Connect(function()
+	speedautofarm:Destroy()
 end)
 
-TextLabel.Parent = Frame_2
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(0.030985916, 0, 0.544444442, 0)
-TextLabel.Size = UDim2.new(0, 335, 0, 35)
-TextLabel.Font = Enum.Font.Highway
-TextLabel.Text = "AntiAFK included. To stop AutoFarm rejoin server by using button above."
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextScaled = true
-TextLabel.TextSize = 14.000
-TextLabel.TextWrapped = true
 
 -- Scripts:
 
-local function AILUK_fake_script() -- Frame.Dragify 
-	local script = Instance.new('LocalScript', Frame)
+
+while wait() do
+	if getgenv().farmer == true then
+		game.Workspace.Events.AddBite1:FireServer("123ase")
+		wait(0.1)
+	end    
+end
+
+
+local function KHCRV_fake_script() -- top.Dragify 
+	local script = Instance.new('LocalScript', top)
 
 	local UIS = game:GetService("UserInputService")
 	function dragify(Frame)
-		dragToggle = nil
-		local dragSpeed = 0
-		dragInput = nil
-		dragStart = nil
-		local dragPos = nil
-		function updateInput(input)
-			local Delta = input.Position - dragStart
-			local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
-			game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
-		end
-		Frame.InputBegan:Connect(function(input)
-			if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
-				dragToggle = true
-				dragStart = input.Position
-				startPos = Frame.Position
-				input.Changed:Connect(function()
-					if input.UserInputState == Enum.UserInputState.End then
-						dragToggle = false
-					end
-				end)
-			end
-		end)
-		Frame.InputChanged:Connect(function(input)
-			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-				dragInput = input
-			end
-		end)
-		game:GetService("UserInputService").InputChanged:Connect(function(input)
-			if input == dragInput and dragToggle then
-				updateInput(input)
-			end
-		end)
+	    dragToggle = nil
+	    local dragSpeed = 0
+	    dragInput = nil
+	    dragStart = nil
+	    local dragPos = nil
+	    function updateInput(input)
+	        local Delta = input.Position - dragStart
+	        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+	        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
+	    end
+	    Frame.InputBegan:Connect(function(input)
+	        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
+	            dragToggle = true
+	            dragStart = input.Position
+	            startPos = Frame.Position
+	            input.Changed:Connect(function()
+	                if input.UserInputState == Enum.UserInputState.End then
+	                    dragToggle = false
+	                end
+	            end)
+	        end
+	    end)
+	    Frame.InputChanged:Connect(function(input)
+	        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+	            dragInput = input
+	        end
+	    end)
+	    game:GetService("UserInputService").InputChanged:Connect(function(input)
+	        if input == dragInput and dragToggle then
+	            updateInput(input)
+	        end
+	    end)
 	end
-
+	
 	dragify(script.Parent)
 end
-coroutine.wrap(AILUK_fake_script)()
+coroutine.wrap(KHCRV_fake_script)()
